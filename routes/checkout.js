6e5b35cf6ab05807c1fd4c5a73eb514bd906f5cb
mike-dalton <michael.j.dalton@gmail.com>
2023-06-07 09:57:10 -0400

    fix duplicity in cart items

--- routes/checkout.js
+++ routes/checkout.js
@@ -2,16 +2,39 @@ const express = require('express');
 const router  = express.Router();
 
 router.post('/', (req, res) => {
-  const { itemId, itemName, itemDescription, itemPrice, quantity } = req.body;
+  const { itemName, itemDescription, itemPrice, quantity } = req.body;
 
   if (!req.session.items) {
     req.session.items = [];
   }
 
-  req.session.items.push(req.body);
+  /**
+   * Check if the cart already contains the item,
+   * if yes, recalculate the quantity
+   * if no, add the item with the given quantity
+   */
+  let foundInCart = false;
+
+  for (const item of req.session.items) {
+    if (item.itemName === req.body.itemName) {
+      foundInCart = true;
+
+      let newQuantity = +item.quantity + +req.body.quantity;
+      let newSubTotal = newQuantity * (+item.itemPrice.slice(3) / +item.quantity);
+
+      item.quantity = newQuantity;
+      item.itemPrice = 'CA$' + newSubTotal.toFixed(2);
+    }
+  }
+
+  if (!foundInCart) {
+    req.session.items.push(req.body);
+  }
+
   console.log('checkout session', req.session);
   res.json(req.session.items);
 });
+
 
 router.get('/', (req, res) => {
 
@@ -19,7 +42,7 @@ router.get('/', (req, res) => {
     req.session.items = [];
   }
 
-  console.log('checkout session GET', req.session);
+  // console.log('checkout session GET', req.session);
   res.json(req.session.items);
 });
 
